08aa27628baa5c99b22db0367f55419ff3532059
Marius <marius.vai@outlook.com>
2020-04-08 15:46:00 +0300

    Add settings

---
+++ src/features/event/user/PeopleDahboard/PeopleDashboard.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const PeopleDashboard = () => {
+    return (
+        <div>
+            <h1>Peaople dashboard</h1>
+        </div>
+    )
+}
+
+export default PeopleDashboard
